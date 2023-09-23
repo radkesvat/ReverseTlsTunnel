8c4e05883727f61fac51500aaad38a31ae21cd42
RadKesvat <mail@nomail.com>
2023-09-23 17:20:18 +0330

    test

--- src/pipe.nim
+++ src/pipe.nim
@@ -8,32 +8,35 @@ type
 
     TransferFlags* = set[DataFlags]
 
-# proc `+`*(x: ptr[uint32],y:int): ptr[uint32] =
+proc `+`*(x: ptr[uint32],y:uint): ptr[uint32] =
+  return cast[ptr[uint32]](cast[uint](x) + y)
 
-#   return cast[ptr[uint32]](cast[uint](x) + y.uint)
-
-# proc encrypt(data: var string, start = 0) =
-#     let len = (data.len() - start) div 4
-#     var address = cast[ptr[uint32]](addr data[start])
-#     for i in 0..<len:
-#         (address+i)[] = `xor`((address+i)[], globals.sh4)
-
-
-# proc decrypt(data: var string) =
-#     var address =cast[ptr[uint32]](addr data[0])
-#     for i in 0 ..< data.len() div 4:
-#         (address+i)[] = `xor`((address+i)[], globals.sh4)
-
-# per byte = consume more cpu (testing)
+# proc `+`(a:pointer,p:pointer): pointer =
+#   result = cast[pointer](cast[int](a) + 1 * sizeof(p))
+  
 proc encrypt(data: var string, start = 0) =
-    for i in start..<data.len():
-        # data[i] = chr(rotateRightBits(uint8(data[i]), globals.sh5))
-        data[i] = chr(uint8(data[i]) xor cast[uint8](globals.sh5))
+    let len = (data.len() - start) div 4
+    var address = cast[ptr[uint32]](addr data[start])
+    for i in 0..<len:
+        (address+i.uint)[] = `xor`((address+i.uint)[], globals.sh4)
+
 
 proc decrypt(data: var string) =
-    for i in 0..<data.len():
-        # data[i] = chr(rotateLeftBits(uint8(data[i]), globals.sh5))
-        data[i] = chr(uint8(data[i]) xor cast[uint8](globals.sh5))
+    var address =cast[ptr[uint32]](addr data[0])
+    for i in 0 ..< (data.len() div 4):
+        (address+i.uint)[] = `xor`((address+i.uint)[], globals.sh4)
+
+# per byte = consume more cpu (testing)
+# proc encrypt(data: var string, start = 0) =
+#     for i in start..<data.len():
+#         # data[i] = chr(rotateRightBits(uint8(data[i]), globals.sh5))
+#         data[i] = chr(cast[uint8](data[i]) xor cast[uint8](globals.sh5))
+
+# proc decrypt(data: var string) =
+#     for i in 0..<data.len():
+#         # data[i] = chr(rotateLeftBits(uint8(data[i]), globals.sh5))
+
+#         data[i] = chr(cast[uint8](data[i]) xor cast[uint8](globals.sh5))
 
 
 # proc muxPack(cid: uint32, port: uint16, data: string): string =
