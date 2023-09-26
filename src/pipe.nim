ede9a6a51279254e15e54232adb67f42aabd8022
RadKesvat <mail@nomail.com>
2023-09-26 17:25:28 +0330

    fix

--- src/pipe.nim
+++ src/pipe.nim
@@ -30,11 +30,13 @@ proc encrypt(data: var string, start = 0) =
     var i: int = start
     while i < data.len():
         data[i] = chr(uint8(data[i]) xor cast[uint8](globals.sh5))
+        i += 1
 
 proc decrypt(data: var string) =
     var i: int = 0
     while i < data.len():
         data[i] = chr(uint8(data[i]) xor cast[uint8](globals.sh5))
+        i += 1
     
 
 # proc muxPack(cid: uint32, port: uint16, data: string): string =
