c92aa3a385a7c866a4587fb8c8f50cef479d26d0
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-10-03 08:48:52 +0330

    update RtTunnel.sh

--- RtTunnel.sh
+++ RtTunnel.sh
@@ -102,28 +102,30 @@ configure_arguments() {
     read -p "Which server do you want to use? (Enter '1' for Iran(internal-server) or '2' for Kharej(external-server) ) : " server_choice
     read -p "Please Enter SNI (default : sheypoor.com): " sni
     sni=${sni:-sheypoor.com}
-    read -p "Do you want to use mux? (yes/no): " use_mux
-    mux_width=2
-    if [ "$use_mux" == "yes" ]; then
-        read -p "Enter mux-width (default: 2): " mux_width
-        mux_width=${mux_width:-2}
-        mux_argument="--mux-width:$mux_width"
-    fi
-
 
     if [ "$server_choice" == "2" ]; then
-        read -p "Please Enter (IRAN IP(internal-server)) : " server_ip
+        read -p "Please Enter IRAN IP(internal-server) : " server_ip
         read -p "Please Enter Password (Please choose the same password on both servers): " password
-        arguments="--kharej --iran-ip:$server_ip --iran-port:443 --toip:127.0.0.1 --toport:multiport --password:$password --sni:$sni $mux_argument--terminate:24"
+        arguments="--kharej --iran-ip:$server_ip --iran-port:443 --toip:127.0.0.1 --toport:multiport --password:$password --sni:$sni --terminate:24"
     elif [ "$server_choice" == "1" ]; then
         read -p "Please Enter Password (Please choose the same password on both servers): " password
+        read -p "Do you want to use mux? (yes/no): " use_mux
+        if [ "$use_mux" == "yes" ]; then
+            read -p "Enter mux-width (default: 2): " mux
+            mux=${mux:-2}
+        elif [ "$use_mux" == "no" ]; then
+            mux=${mux:-1}
+        else
+            echo "Invalid choice for mux. Please enter 'yes' or 'no'."
+            exit 1
+        fi
         read -p "Do you want to use fake upload? (yes/no): " use_fake_upload
         if [ "$use_fake_upload" == "yes" ]; then
             read -p "Enter upload-to-download ratio (e.g., 5 for 5:1 ratio): " upload_ratio
             upload_ratio=$((upload_ratio - 1))
-            arguments="--iran --lport:23-65535 --sni:$sni --password:$password $mux_argument--noise:$upload_ratio --terminate:24"
+            arguments="--iran --lport:23-65535 --sni:$sni --password:$password --mux-width:$mux --noise:$upload_ratio --terminate:24"
         else
-            arguments="--iran --lport:23-65535 --sni:$sni --password:$password $mux_argument--terminate:24"
+            arguments="--iran --lport:23-65535 --sni:$sni --password:$password --mux-width:$mux --terminate:24"
         fi
     else
         echo "Invalid choice. Please enter '1' or '2'."
@@ -176,24 +178,17 @@ configure_arguments2() {
     read -p "Which server do you want to use? (Enter '1' for Iran(internal-server) or '2' for Kharej(external-server) ) : " server_choice
     read -p "Please Enter SNI (default : sheypoor.com): " sni
     sni=${sni:-sheypoor.com}
-    read -p "Do you want to use mux? (yes/no): " use_mux
-    mux_width=2
-    if [ "$use_mux" == "yes" ]; then
-        read -p "Enter mux-width (default: 2): " mux_width
-        mux_width=${mux_width:-2}
-        mux_argument="--mux-width:$mux_width"
-    fi
 
     if [ "$server_choice" == "2" ]; then
         read -p "Is this your main server (VPN server)? (yes/no): " is_main_server
-        read -p "Please Enter (IRAN IP(internal-server)) : " server_ip
+        read -p "Please Enter IRAN IP(internal-server) : " server_ip
         read -p "Please Enter Password (Please choose the same password on both servers): " password
 
         if [ "$is_main_server" == "yes" ]; then
-            arguments="--kharej --iran-ip:$server_ip --iran-port:443 --toip:127.0.0.1 --toport:multiport --password:$password --sni:$sni $mux_argument--terminate:24"
+            arguments="--kharej --iran-ip:$server_ip --iran-port:443 --toip:127.0.0.1 --toport:multiport --password:$password --sni:$sni --terminate:24"
         elif [ "$is_main_server" == "no" ]; then
             read -p "Enter your main IP (VPN Server):  " main_ip
-            arguments="--kharej --iran-ip:$server_ip --iran-port:443 --toip:$main_ip --toport:multiport --password:$password --sni:$sni $mux_argument--terminate:24"
+            arguments="--kharej --iran-ip:$server_ip --iran-port:443 --toip:$main_ip --toport:multiport --password:$password --sni:$sni --terminate:24"
         else
             echo "Invalid choice for main server. Please enter 'yes' or 'no'."
             exit 1
@@ -201,13 +196,23 @@ configure_arguments2() {
 
     elif [ "$server_choice" == "1" ]; then
         read -p "Please Enter Password (Please choose the same password on both servers): " password
+        read -p "Do you want to use mux? (yes/no): " use_mux
+        if [ "$use_mux" == "yes" ]; then
+            read -p "Enter mux-width (default: 2): " mux
+            mux=${mux:-2}
+        elif [ "$use_mux" == "no" ]; then
+            mux=${mux:-1}
+        else
+            echo "Invalid choice for mux. Please enter ' yes' or 'no'."
+            exit 1
+        fi
         read -p "Do you want to use fake upload? (yes/no): " use_fake_upload
         if [ "$use_fake_upload" == "yes" ]; then
             read -p "Enter upload-to-download ratio (e.g., 5 for 5:1 ratio): " upload_ratio
             upload_ratio=$((upload_ratio - 1))
-            arguments="--iran --lport:23-65535 --password:$password --sni:$sni $mux_argument--noise:$upload_ratio --terminate:24"
+            arguments="--iran --lport:23-65535 --password:$password --sni:$sni --mux-width:$mux --noise:$upload_ratio --terminate:24"
         else
-            arguments="--iran --lport:23-65535 --password:$password --sni:$sni $mux_argument--terminate:24"
+            arguments="--iran --lport:23-65535 --password:$password --sni:$sni --mux-width:$mux --terminate:24"
         fi
         
         num_ips=0
