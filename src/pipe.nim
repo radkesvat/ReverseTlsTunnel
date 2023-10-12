e917a6431805b60cc0029a1388bbd22b64c3f807
RadKesvat <mail@nomail.com>
2023-10-12 03:33:50 +0330

    test

--- src/pipe.nim
+++ src/pipe.nim
@@ -1,5 +1,5 @@
 from globals import nil
-import random, strutils,bitops
+import random, strutils, bitops
 
 type
     DataFlags* {.size: sizeof(uint8), pure.} = enum
@@ -42,7 +42,7 @@ type
 #         data[i] = chr(uint8(data[i]) xor cast[uint8](globals.sh5))
 #         i += 1
 
-proc encrypt(data: var string, start:int = 0, nbytes: int = data.len()) =
+proc encrypt(data: var string, start: int = 0, nbytes: int = data.len()) =
     var i: int = start
     let loopmax = min(data.len(), start + nbytes)
     while i < loopmax:
@@ -68,14 +68,15 @@ proc flagForSend*(data: var string, flags: TransferFlags) =
     if data.len < width: data.setLen(width)
 
     var size: uint16 = data.len.uint16 - globals.full_tls_record_len.uint16
-    
-    let dif:uint8 = (size mod 16).uint8
+
+    let dif: uint8 = 16 - (size mod 16).uint8
+    if dif == 16: dif = 0
     data.setLen data.len + dif.int
     size += dif
 
     copyMem(addr size, addr data[0 + globals.tls13_record_layer.len()], sizeof(size))
 
-    let e_flags: uint8 = bitand(cast[uint8](flags),0xF) xor size.uint8
+    let e_flags: uint8 = bitand(cast[uint8](flags), 0xF) xor size.uint8
 
 
     copyMem(addr data[0 + globals.full_tls_record_len.int+sizeof(uint16)+sizeof(uint16)], addr e_flags, sizeof(e_flags))
@@ -87,7 +88,8 @@ proc packForSend*(data: var string, cid: uint16, port: uint16, flags: TransferFl
     var size: uint16 = data.len().uint16 - globals.full_tls_record_len.uint16
 
 
-    let dif:uint8 = (size mod 16).uint8
+    let dif: uint8 = 16 - (size mod 16).uint8
+    if dif == 16: dif = 0
     data.setLen data.len + dif.int
     size += dif
 
@@ -99,8 +101,8 @@ proc packForSend*(data: var string, cid: uint16, port: uint16, flags: TransferFl
     let e_port: uint16 = port xor size
 
 
-    var e_flags: uint8 = bitand(cast[uint8](flags),0xF) 
-    e_flags = bitor((dif shl 4),e_flags)
+    var e_flags: uint8 = bitand(cast[uint8](flags), 0xF)
+    e_flags = bitor((dif shl 4), e_flags)
     e_flags = e_flags xor size.uint8
 
     copyMem(addr data[0 + globals.full_tls_record_len.int], addr e_cid, sizeof(e_cid))
@@ -116,20 +118,22 @@ proc closeSignalData*(cid: uint16): string =
     let flags: TransferFlags = {}
 
     let width = globals.full_tls_record_len.int+sizeof(port)+sizeof(cid) + sizeof(uint8)
-    var data = newStringOfCap(16);data.setLen(width)
+    var data = newStringOfCap(16); data.setLen(width)
 
 
     var size: uint16 = data.len().uint16 - globals.full_tls_record_len.uint16
 
-    let dif:uint8 = (size mod 16).uint8
+    let dif: uint8 = 16 - (size mod 16).uint8
+    if dif == 16: dif = 0
     data.setLen data.len + dif.int
     size += dif
-    echo "close signal diff: ",dif
+    
+    echo "close signal diff: ", dif
 
     # let size: uint16 = sizeof(port)+sizeof(cid) + sizeof(uint8)
     let e_cid: uint16 = cid xor size
-    var e_flags: uint8 = bitand(cast[uint8](flags),0xF) 
-    e_flags = bitor((dif shl 4),e_flags)
+    var e_flags: uint8 = bitand(cast[uint8](flags), 0xF)
+    e_flags = bitor((dif shl 4), e_flags)
     e_flags = e_flags xor size.uint8
 
     copyMem(addr data[0], addr globals.tls13_record_layer[0], globals.tls13_record_layer.len())
